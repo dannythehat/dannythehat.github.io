7f78d04e0a414121837add7bba39ba1b4408ed36
dannythehat <dannythehat2@gmail.com>
2026-01-08 17:43:11 +0200

    Add Privacy and Terms routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,8 @@ import StoryDetail from "./pages/StoryDetail";
 import UKStays from "./pages/UKStays";
 import UKStayDetail from "./pages/UKStayDetail";
 import About from "./pages/About";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -38,6 +40,8 @@ const App = () => (
               <Route path="/stories" element={<Stories />} />
               <Route path="/stories/:slug" element={<StoryDetail />} />
               <Route path="/about" element={<About />} />
+              <Route path="/privacy" element={<Privacy />} />
+              <Route path="/terms" element={<Terms />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
