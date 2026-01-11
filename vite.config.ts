9d98d0d8a4801482937c37e639691d2e1cb04da2
dannythehat <dannythehat2@gmail.com>
2026-01-11 18:30:24 +0200

    Update vite config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: mode === "production" ? "/beyond-ordinary-adventures/" : "/",
 }));
